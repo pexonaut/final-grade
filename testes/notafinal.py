272c96f1889aa6ae463e25b8d4fe881c641750ab
pexonaut <enzobuenodealmeida@gmail.com>
2022-10-02 02:11:20 -0300

    Consertei as listas em conflito

--- testes/notafinal.py
+++ testes/notafinal.py
@@ -6,19 +6,19 @@ g2 = int(input("Nota da g2: "))
 g3 = int(input("Nota da g3: "))
 pe = int(input("Pontos extras e diversificadas: "))
 
-#Pegar as duas notas maiores-
-    
-#Maior nota:
+# Pegar as duas notas maiores-
+
+# Maior nota:
 lista1 = [g1, g2, g3]
 nt1 = max(lista1)
 
-#Segunda maior nota:
+# Segunda maior nota:
 nt2 = (heapq.nlargest(2, lista1)[-1])
 
-#Média entre elas/soma de pontos-
+# Média entre elas/soma de pontos-
 mdp = round((nt1 + nt2)/2) + pe
 
-#Calcular outro trimestre-
+# Calcular outro trimestre-
 escolha = str(input('''Deseja adicionar outro trimestre? 
 [S/N]: '''))
 
@@ -29,18 +29,18 @@ if escolha == "S":
         globo2 = int(input("Nota da g2: "))
         globo3 = int(input("Nota da g3: "))
         pxe = int(input("Pontos extras e diversificadas: "))
-        
-        #Maior nota:
+
+        # Maior nota:
         lista2 = [globo1, globo2, globo3]
         nota1 = max(lista2)
 
-        #Segunda maior nota:
-        nota2 = (heapq.nlargest(2, lista1)[-1])
-        
-        #Média entre elas/soma de pontos-
+        # Segunda maior nota:
+        nota2 = (heapq.nlargest(2, lista2)[-1])
+
+        # Média entre elas/soma de pontos-
         media = round((nota1 + nota2)/2) + pxe
-        
-        #Calcular quantos pontos faltam para passar de ano
+
+        # Calcular quantos pontos faltam para passar de ano
         ptf = 180 - (media + mdp)
         if ptf <= 0:
             print('''Media do 1° trimestre: {}
@@ -50,9 +50,8 @@ Você já passou de ano, parabéns!'''.format(mdp, media))
             print('''Media do 1° trimestre: {}
 Media do 2° trimestre: {}
 Faltam {} pontos para passar de ano!'''.format(mdp, media, ptf))
-        
-        break;
-else:
-    print('A média final do aluno(a) foi de [{}] pontos no trimestre'.format(mdp))
 
-        
+        break
+else:
+    print(
+        'A média final do aluno(a) foi de [{}] pontos no trimestre'.format(mdp))
