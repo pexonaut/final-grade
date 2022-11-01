c01b43cc552b89a6d0d9ee2d16a7b8209bf51d89
pexonaut <enzobuenodealmeida@gmail.com>
2022-11-01 16:06:40 -0300

    finalizado(colocar exemplos no print db)

--- nota_final_bd.py
+++ nota_final_bd.py
@@ -2,7 +2,8 @@ import heapq
 import sqlite3
 import time
 import biblioteca_funções
-#peixonauta ;)
+# peixonauta ;)
+
 
 def main():
     conexao = sqlite3.connect('banco_de_dados_anchieta.db')
@@ -14,11 +15,11 @@ def main():
     calcular_notas = 1
     ver_notas = 2
 
-
     if inserir_menu == calcular_notas:
         while True:
             print("-" * 28)
-            print('\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]')
+            print(
+                '\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]')
 
             materia_mat = 1
             materia_geo = 2
@@ -27,7 +28,8 @@ def main():
             materia_qui = 5
             materia_fis = 6
 
-            materia_escolhida = int(input("Escolha a materia calculada (Precisa ser um numero): \033[m"))
+            materia_escolhida = int(
+                input("Escolha a materia calculada (Precisa ser um numero): \033[m"))
 
             if ((materia_escolhida > 0) and (materia_escolhida <= 6)):
 
@@ -60,22 +62,25 @@ def main():
                 print("-" * 28)
 
                 if ptf <= 0:
-                    print('''\033[32mMedia do 1° trimestre: {}\nMedia do 2° trimestre: {}\nVocê já passou de ano, parabéns!'''.format(mdp, media))
+                    print('''\033[32mMedia do 1° trimestre: {}\nMedia do 2° trimestre: {}\nVocê já passou de ano, parabéns!'''.format(
+                        mdp, media))
                     ptf = 0
                     rapariga = "passou"
-                    
+
                 else:
-                    print('''\033[31mMedia do 1° trimestre: {}\nMedia do 2° trimestre: {}\nFaltam {} pontos para passar de ano!'''.format(mdp, media, ptf))
+                    print('''\033[31mMedia do 1° trimestre: {}\nMedia do 2° trimestre: {}\nFaltam {} pontos para passar de ano!'''.format(
+                        mdp, media, ptf))
                     rapariga = "não passou"
-            
+
             else:
                 print("\033[31mOpção Invalida\033[m")
-                
-                repe = input("\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
-                
+
+                repe = input(
+                    "\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
+
                 if repe == "S":
                     main()
-                
+
                 else:
                     print("Programa encerrado :(")
                     exit()
@@ -105,44 +110,46 @@ def main():
                 else:
                     cursor.execute(
                         f"INSERT INTO Fisica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
-                    conexao.commit()    
+                    conexao.commit()
 
-                repe = input("\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
-                
+                repe = input(
+                    "\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
+
                 if repe == "S":
                     main()
-                
+
                 else:
                     print("Programa encerrado :(")
                     exit()
 
-                        
                 break
             break
     elif inserir_menu == ver_notas:
         while True:
-            print("-" * 28)  
-            print('\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]\033[m')
-            
+            print("-" * 28)
+            print(
+                '\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]\033[m')
+
             materia_mat = 1
             materia_geo = 2
             materia_hist = 3
             materia_port = 4
             materia_qui = 5
             materia_fis = 6
-            materia_escolhida = int(input("Escolha a materia que vai ser visualizada (Precisa ser um numero): "))
+            materia_escolhida = int(
+                input("Escolha a materia que vai ser visualizada (Precisa ser um numero): "))
 
             if ((materia_escolhida > 0) and (materia_escolhida <= 6)):
                 while True:
 
-                    print("-" * 28)  
+                    print("-" * 28)
 
                     print("\033[35mCarregando 'banco_de_dados_anchieta'\033[m")
                     print(' ')
                     time.sleep(2)
-                    
 
-                    print("\033[33mModelo para visualização de dados:\n('Média do 1° tri', 'Média do 2° tri', 'Situação final', 'Pontos para passar')")
+                    print(
+                        "\033[33mModelo para visualização de dados:\n('Média do 1° tri', 'Média do 2° tri', 'Pontos para passar', 'Situação Final')")
                     print(' ')
                     print('---Notas---\033[m')
 
@@ -164,42 +171,46 @@ def main():
                     else:
                         for row in cursor.execute("SELECT f_mdp, f_media, rapariga, f_pf FROM Fisica"):
                             print(row)
-                
-                    repe = input("\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
-                
+
+                    repe = input(
+                        "\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
+
                     if repe == "S":
                         main()
-                
+
                     else:
                         print("Programa encerrado :(")
                         exit()
 
                     break
-            
+
             else:
                 print("\033[31mOpção Invalida\033[m")
 
-                repe = input("\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
-                
+                repe = input(
+                    "\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
+
                 if repe == "S":
                     main()
-                
+
                 else:
                     print("Programa encerrado :(")
                     exit()
-            
+
             break
-    
+
     else:
         print("\033[31mOpção Invalida\033[m")
 
-        repe = input("\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
-                
+        repe = input(
+            "\033[35mDeseja reiniciar o programa? [S/N]\033[m").upper()
+
         if repe == "S":
             main()
-                
+
         else:
             print("Programa encerrado :(")
             exit()
+
 
 main()
