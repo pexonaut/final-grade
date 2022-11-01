b8de3984bdab4950ff25b133884fd30127f0b142
pexonaut <enzobuenodealmeida@gmail.com>
2022-11-01 17:17:40 -0300

    ultima modificações

--- nota_final_bd.py
+++ nota_final_bd.py
@@ -2,7 +2,6 @@ import heapq
 import sqlite3
 import time
 import biblioteca_funções
-# peixonauta ;)
 
 
 def main():
@@ -19,7 +18,7 @@ def main():
         while True:
             print("-" * 28)
             print(
-                '\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]')
+                '\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]\nBiologia [7]')
 
             materia_mat = 1
             materia_geo = 2
@@ -27,19 +26,23 @@ def main():
             materia_port = 4
             materia_qui = 5
             materia_fis = 6
+            materia_bio = 7
 
             materia_escolhida = int(
                 input("Escolha a materia calculada (Precisa ser um numero): \033[m"))
 
-            if ((materia_escolhida > 0) and (materia_escolhida <= 6)):
+            if ((materia_escolhida > 0) and (materia_escolhida <= 7)):
 
                 print('-------1° trimestre--------')
                 g1 = int(input("Nota da g1: "))
                 g2 = int(input("Nota da g2: "))
                 g3 = int(input("Nota da g3: "))
-                pe = int(input("Pontos extras e diversificadas: "))
+                
+                p1 = int(input("Diversificada: "))
+                p2 = int(input("Pontos extras: "))
 
                 lista1 = [g1, g2, g3]
+                pe = p1 + p2
                 nt1 = max(lista1)
                 nt2 = (heapq.nlargest(2, lista1)[-1])
                 mdp = round((nt1 + nt2)/2) + pe
@@ -48,15 +51,16 @@ def main():
                 globo1 = int(input("Nota da g1: "))
                 globo2 = int(input("Nota da g2: "))
                 globo3 = int(input("Nota da g3: "))
-                pxe = int(input("Pontos extras e diversificadas: "))
+                
+                px1 = int(input("Diversificadas: "))
+                px2 = int(input("Pontos extras: "))
 
                 lista2 = [globo1, globo2, globo3]
+                
+                pxe = px1 + px2
                 nota1 = max(lista2)
-
                 nota2 = (heapq.nlargest(2, lista2)[-1])
-
                 media = round((nota1 + nota2)/2) + pxe
-
                 ptf = 180 - (media + mdp)
 
                 print("-" * 28)
@@ -85,31 +89,35 @@ def main():
                     print("Programa encerrado :(")
                     exit()
 
-            while ((materia_escolhida > 0) and (materia_escolhida <= 6)):
+            while ((materia_escolhida > 0) and (materia_escolhida <= 7)):
 
                 if materia_escolhida == materia_mat:
                     cursor.execute(
-                        f"INSERT INTO Matematica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
+                        f"INSERT INTO Matematica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
                     conexao.commit()
                 elif materia_escolhida == materia_geo:
                     cursor.execute(
-                        f"INSERT INTO Geografia VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
+                        f"INSERT INTO Geografia VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
                     conexao.commit()
                 elif materia_escolhida == materia_hist:
                     cursor.execute(
-                        f"INSERT INTO Historia VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
+                        f"INSERT INTO Historia VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
                     conexao.commit()
                 elif materia_escolhida == materia_port:
                     cursor.execute(
-                        f"INSERT INTO Português VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
+                        f"INSERT INTO Português VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
                     conexao.commit()
                 elif materia_escolhida == materia_qui:
                     cursor.execute(
-                        f"INSERT INTO Quimica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
+                        f"INSERT INTO Quimica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
+                    conexao.commit()
+                elif materia_escolhida == materia_fis:
+                    cursor.execute(
+                        f"INSERT INTO Fisica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
                     conexao.commit()
                 else:
                     cursor.execute(
-                        f"INSERT INTO Fisica VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{ptf}', '{rapariga}')")
+                        f"INSERT INTO Biologia VALUES ('{nt1}', '{nt2}', '{mdp}', '{nota1}', '{nota2}', '{media}', '{rapariga}', '{ptf}')")
                     conexao.commit()
 
                 repe = input(
@@ -128,7 +136,7 @@ def main():
         while True:
             print("-" * 28)
             print(
-                '\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]\033[m')
+                '\033[33mMatematica [1]\nGeografia [2]\nHistoria [3]\nPortuguês [4]\nQuimica [5]\nFisica [6]\nBiologia [7]\033[m')
 
             materia_mat = 1
             materia_geo = 2
@@ -136,10 +144,12 @@ def main():
             materia_port = 4
             materia_qui = 5
             materia_fis = 6
+            materia_bio = 7
+
             materia_escolhida = int(
                 input("Escolha a materia que vai ser visualizada (Precisa ser um numero): "))
 
-            if ((materia_escolhida > 0) and (materia_escolhida <= 6)):
+            if ((materia_escolhida > 0) and (materia_escolhida <= 7)):
                 while True:
 
                     print("-" * 28)
@@ -149,7 +159,7 @@ def main():
                     time.sleep(2)
 
                     print(
-                        "\033[33mModelo para visualização de dados:\n('Média do 1° tri', 'Média do 2° tri', 'Pontos para passar', 'Situação Final')")
+                        "\033[33mModelo para visualização de dados:\n('Média do 1° tri', 'Média do 2° tri', 'Situação final', 'Pontos para passar')")
                     print(' ')
                     print('---Notas---\033[m')
 
@@ -168,8 +178,11 @@ def main():
                     elif materia_escolhida == materia_qui:
                         for row in cursor.execute("SELECT q_mdp, q_media, rapariga, q_pf FROM Quimica"):
                             print(row)
-                    else:
+                    elif materia_escolhida == materia_fis:
                         for row in cursor.execute("SELECT f_mdp, f_media, rapariga, f_pf FROM Fisica"):
+                            print(row)
+                    else:
+                        for row in cursor.execute("SELECT b_mdp, b_media, rapariga, b_pf FROM Biologia"):
                             print(row)
 
                     repe = input(
